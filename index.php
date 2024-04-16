f68e32e4f5dafb086085aaf409c8e7d4c5a5917d
Jack Tabb <jacktabb@Jacks-MacBook-Pro-2.local>
2024-04-16 14:48:16 -0500

    Added index

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+<style>
+	.top{
+		color: blue;
+		text-align: center;
+	}
+</style>
+<head>
+	<title>Group Project</title>
+</head>
+<body>
+	<h1 class='top'> CSCI 466 Project</h1>
+</body>
+</html>
+
